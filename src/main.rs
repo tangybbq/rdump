c161aee7c002922db4f76924c253414d17805345
David Brown <davidb@davidb.org>
2021-02-16 19:40:28 -0700

    Add cli parsing and some random stuff

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 //! Backup driver
 
 use anyhow::Result;
+use clap::{
+    App,
+    load_yaml,
+};
+use std::{
+    path::Path,
+};
 use zfs::Zfs;
 
 mod checked;
@@ -8,7 +15,41 @@ mod zfs;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let fs = Zfs::new(None, "lint").await?;
-    println!("{:#?}", fs);
+    if false {
+        wasy().await?;
+    }
+
+    let yaml = load_yaml!("cli.yaml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    println!("{:#?}", matches);
+
+    let config = matches.value_of("config").unwrap_or("rdump.yaml");
+    println!("config: {:?}", config);
+
+    if let Some(matches) = matches.subcommand_matches("clone") {
+        let volume = matches.value_of("VOLUME").unwrap();
+        println!("volume: {:?}", volume);
+    }
+
+    Ok(())
+}
+
+async fn wasy() -> Result<()> {
+    let fs = Zfs::new(None, "da2021").await?;
+    // println!("{:#?}", fs);
+
+    // Scan each filesystem that has a mountpoint, looking for a
+    // .zfssync.yml file.
+    let mut valid = vec![];
+    for fs in &fs.filesystems {
+        let base = Path::new(&fs.mount);
+        let work = base.join(".zfssync.yml");
+        match tokio::fs::metadata(&work).await {
+            Ok(_) => valid.push(fs),
+            Err(_) => (),
+        }
+    }
+    println!("Valid: {:?}", valid);
     Ok(())
 }
